a1cdb50577592616e3203dc8e7dd6d61c92c521e
sungtv <sungtv8888@gmail.com>
2026-09-22 23:15:09 +0700

    update footer copy right page login/admin

--- admin/language/vi-vn/common/footer.php
+++ admin/language/vi-vn/common/footer.php
@@ -1,4 +1,4 @@
 <?php
 // Text
-$_['text_footer'] = '<a href="https://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_footer'] = '<a href="https://cxhome.vn">CX Home</a> &copy; 2025-' . date('Y') . ' All Rights Reserved.';
 $_['text_version'] = 'Version %s';
